0993259274e6d9af24937904d6ae837d6029b328
arun.nadger <arun.nadger@v-key.com>
2019-02-19 10:08:28 +0800

    Changes

--- src/main/java/com/test/string/accumulator/StringAccumulator.java
+++ src/main/java/com/test/string/accumulator/StringAccumulator.java
@@ -2,21 +2,23 @@ package com.test.string.accumulator;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class StringAccumulator implements Accumulator<String> {
 
     public int add(String numbers) throws AccumulatorException{
-        if(numbers == null) throw new AccumulatorException("Null input");
+        if(null == numbers) throw new AccumulatorException("Input cannot be null");
         if(numbers.isEmpty()) return 0;
         return Arrays.stream(split(numbers)).sum();
     }
 
-    private int[] split(String numbers){
+    private int[] split(String numbers) throws AccumulatorException {
         return Arrays.stream(retrieveDelimitersAndNumbers(numbers)).mapToInt(Integer::parseInt).toArray();
     }
 
-    private String[] retrieveDelimitersAndNumbers(String numbers){
+    private String[] retrieveDelimitersAndNumbers(String numbers) throws AccumulatorException {
         String delimeters = ",\n";
         if(numbers.startsWith("//")){
             delimeters = StringUtils.substringBetween(numbers,"//","\n");
@@ -24,7 +26,23 @@ public class StringAccumulator implements Accumulator<String> {
             String[] delimiterArray = StringUtils.split(delimeters,'|');
             delimeters = StringUtils.join(delimiterArray,"");
         }
-        return StringUtils.split(numbers,delimeters);
+        return parseArray(Arrays.asList(StringUtils.split(numbers,delimeters)));
+    }
+
+    private String[] parseArray(List<String> numbers) throws AccumulatorException {
+        List<Integer> negativeNumbers = new ArrayList<>();
+        List<String> finalNumbers = new ArrayList<>();
+        for(String number: numbers){
+            int iNumber = Integer.parseInt(number);
+            if(iNumber < 0) {
+                negativeNumbers.add(iNumber);
+                continue;
+            }
+            if(iNumber > 1000) continue;
+            finalNumbers.add(String.valueOf(iNumber));
+        }
+        if(!negativeNumbers.isEmpty()) throw new AccumulatorException("negatives not allowed, "+negativeNumbers);
+        return finalNumbers.toArray(new String[0]);
     }
 
 }
--- src/test/java/com/test/string/accumulator/StringAccumulatorTest.java
+++ src/test/java/com/test/string/accumulator/StringAccumulatorTest.java
@@ -48,4 +48,35 @@ public class StringAccumulatorTest {
         Assert.assertEquals(stringAccumulator.add("//*|%\n1*2%3"),6
         );
     }
+
+    @Test
+    public void testSix() throws AccumulatorException{
+        StringAccumulator stringAccumulator = new StringAccumulator();
+        Assert.assertEquals(stringAccumulator.add("//***|%%\n1*2%3"),6
+        );
+    }
+
+    @Test(expected = AccumulatorException.class)
+    public void testSeven() throws AccumulatorException{
+        StringAccumulator stringAccumulator = new StringAccumulator();
+        stringAccumulator.add("-1,-2,3");
+    }
+
+    @Test
+    public void testEight() throws AccumulatorException{
+        StringAccumulator stringAccumulator = new StringAccumulator();
+        Assert.assertEquals(stringAccumulator.add("1,1001,2,3"),6
+        );
+    }
+
+    @Test
+    public void testNine() {
+        StringAccumulator stringAccumulator = new StringAccumulator();
+        try {
+            stringAccumulator.add("-1,-2,3");
+        } catch (AccumulatorException e) {
+            Assert.assertEquals(e.getMessage(),"negatives not allowed, [-1, -2]");
+        }
+    }
+
 }
